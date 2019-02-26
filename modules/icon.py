c4d4f3ca04a5543d43da71b0bd291fa5fbfbac4e
Matthew Odle <matt.odle@tdstelecom.com>
2019-02-25 22:52:06 -0600

    assign icon points

--- modules/icon.py
+++ modules/icon.py
@@ -8,7 +8,8 @@ fp.close()
 relations = ["positive", "conflicted", "negative"]
 
 MAX_ICONS = 3
-MIN_ICONS = 1
+MIN_ICONS = 2
+POINTS = 3
 
 def main():
     # because we want to delete entries to avoid duplicates
@@ -16,15 +17,28 @@ def main():
     icons = icon_data.copy()
     chosen_icons = []
 
+    assigned_points = 0
+
     while len(chosen_icons) < num_icons:
         icon_index = random.randint(0, len(icons)-1)
         relations_index = random.randint(0, len(relations)-1)
-        icon = (icons[icon_index], relations[relations_index])
+        icon_points = get_points(len(chosen_icons), num_icons, assigned_points)
+        assigned_points += icon_points
+        icon = (icons[icon_index], relations[relations_index], 'points: {}'.format(icon_points))
         # avoids duplicates
         del icons[icon_index]
         chosen_icons.append(icon)
 
     return chosen_icons
+
+def get_points(current_icon, total_icons, assigned_points):
+    if total_icons == 3:
+        return 1
+    if total_icons == 2:
+        if current_icon == 0:
+            return random.randint(1, 2)
+        else:
+            return POINTS - assigned_points
 
 if __name__ == "__main__":
     result = main()
